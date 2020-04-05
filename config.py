2cdcef16056f9674aadaa0b28913941f66b4bd2c
Sergioarv <sarv9208@hotmail.com>
2020-04-05 11:22:40 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 import os
 
-DBURL = 'sqlite:///'+ os.path.abspath('./dbsgc.db')
+# Base en Produccion Online
+DBURL = 'postgres://wviqnqyhdeqzse:c870122ac441bd00ce0c905849e29247b819822df9e07dd2f8eb01270e6f84f9@ec2-3-91-112-166.compute-1.amazonaws.com:5432/d9dh9aftbst1tp'
+# Base Local
+# DBURL = 'sqlite:///'+ os.path.abspath('./dbsgc.db')
 
 class Config(object):
     DEBUG = False
