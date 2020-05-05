8e7a3c618ecbc648aaae5add726e12e76e9fad8b
Sergioarv <sarv9208@hotmail.com>
2020-05-05 14:58:54 -0500

    fixed graficos ruta

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -8,7 +8,7 @@ def dashboard_index():
     return render_template('/dashboard/dashboard.html')
 
 @app.route('/solicitudes/<string:d>')
-def prueba(d):
+def solicitudes(d):
     tittle ='Cantidad de Solicitudes por Tipo'
     data = []
     valor = ['Solicitado','Rechazado','Procesado','Aceptado']
@@ -19,7 +19,7 @@ def prueba(d):
     return render_template('/dashboard/piechart.html', mydata = data, valor = valor, tittle = tittle, dashboard = d)
 
 @app.route('/solicitudesBar/<string:d>')
-def prueba2(d):
+def solicitudesBar(d):
     tittle ='Cantidad de Solicitudes por Tipo'
     data = []
     c = ['#0040FF','#088A4B','#FF0000','#FF8000']
@@ -31,7 +31,7 @@ def prueba2(d):
     return render_template('/dashboard/barchart.html', mydata = data, valor = valor, tittle = tittle, c = c, dashboard = d)
 
 @app.route('/tiempo/<string:d>')
-def prueba2(d):
+def tiempo(d):
     tittle ='Tiempo en Responder Solicitudes'
     data = []
     valor = ['Solicitado','Rechazado','Procesado','Aceptado']
