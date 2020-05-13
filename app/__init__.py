a3d3bf3b36e2e662010c0b7902b9e31e3c775824
Sergioarv <sarv9208@hotmail.com>
2020-05-13 14:20:46 -0500

    Fixed Config
    Error en la configuracion, configuración de Desarrollo, no Produccion, Ni acceso a base de datos de postgres

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-#app.config.from_object('config.ProductionConfig')
+app.config.from_object('config.ProductionConfig')
 #app.config.from_object('config.DevelopmentConfig')
-app.config.from_object('config.TestConfig')
+#app.config.from_object('config.TestConfig')
 
 email_emp = app.config['MAIL_USERNAME']
 password_emp = app.config['MAIL_PASSW']
