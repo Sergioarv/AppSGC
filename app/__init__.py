04053728086d8ad5ed8c6e66551a9f19a1b972b2
Sergioarv <sarv9208@hotmail.com>
2020-05-22 10:02:23 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-#app.config.from_object('config.ProductionConfig')
-app.config.from_object('config.DevelopmentConfig')
+app.config.from_object('config.ProductionConfig')
+#app.config.from_object('config.DevelopmentConfig')
 #app.config.from_object('config.TestConfig')
 
 email_emp = app.config['MAIL_USERNAME']
